252ac40d07a184a0674c440e8c149fbd960af12f
AceSolleza <113578060+AceSolleza@users.noreply.github.com>
2022-09-14 21:53:36 -1200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        {
+            String Firstname = "Ace Raniel";
+            String Lastname = "Solleza";
+            int age = 20;
+            float gpa = 1.75f;
+            char bloodtype = 'O';
+
+            System.out.println(" Hi, My name is " + Firstname + "  " + Lastname);
+
+            System.out.println("I am " + age + "years old");
+
+            System.out.println("My GPA is " + gpa);
+
+            System.out.println("My Bloodtype is " + bloodtype);
+
+        }
+    }
+
+}
